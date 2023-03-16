1302a15211904a3282454c0daeba0baaa27a0858
Hao, Yi A <yi.a.hao@accenture.com>
2023-03-16 09:14:37 -0500

    Initial Commit

---
+++ 20230316-YiHao-NYCSchools/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  20230316-YiHao-NYCSchools
+//
+//  Created by yi.a.hao on 3/16/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
